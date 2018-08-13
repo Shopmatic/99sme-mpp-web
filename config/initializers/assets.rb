e9fa5a15c7ea5fcb8f4b9b69c8960265b9bf5e72
Jidesh Gopalan <Jidesh.Gopalan@Gmail.com>
2018-08-13 11:02:30 +0530

    initial commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,13 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths += Dir[ Rails.root.join('node_modules'),
+                                              Rails.root.join('vendor/*')
+                                            ].sort_by { |dir| -dir.size }
+
+Rails.application.config.assets.precompile += %w( custom/index.scss )
+
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
