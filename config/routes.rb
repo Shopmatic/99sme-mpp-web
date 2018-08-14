6ebcda424c925cd68db5cfc3196063c3f44d304b
Jidesh Gopalan <Jidesh.Gopalan@Gmail.com>
2018-08-14 11:44:31 +0530

    add product and merchant ar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  get 'store', to: 'store#index'
-  get 'products', to: 'products#index'
+  root to: "home#index"
+  get "/store/:name" => "store#index"
+  get "products", to: "products#index"
 end
