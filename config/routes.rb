ef40dd08840a32d662aea759688aad1e8041a50f
vikasputty <vikas.putty@goshopmatic.com>
2018-08-20 00:14:57 +0530

    UI fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,4 @@ Rails.application.routes.draw do
   root to: "home#index"
   get "/store/:name" => "store#index"
   
-  
-  resources :products do
-    member do 
-      get :details
-    end
-  end
-
-
 end
