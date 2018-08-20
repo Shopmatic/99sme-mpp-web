6781b3170357a9fb795e344f2668f31d3309b88b
Jidesh Gopalan <Jidesh.Gopalan@Gmail.com>
2018-08-20 14:25:39 +0530

    product details helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,3 +1,15 @@
 module ProductsHelper
-  
+
+  def product_image(product, image, order_no, order, active)
+   
+    html = <<-PRODUCT_IMAGES
+        <div class="#{active} item carousel-item" data-slide-number="#{order_no}">
+                  <div id='#{order}-#{product.id}'>
+                    <img  class="img-fluid" alt="" src="#{!(image.blank?)  ? image : 'http://placehold.it/600x600'}">
+                  </div>
+         </div>
+    PRODUCT_IMAGES
+    html
+  end
+
 end
